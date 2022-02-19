3291f72d858a52ae61790176fdc5dfaaac09234b
lingwang <lingwang@xiaoe-tech.com>
2022-02-19 19:06:21 +0800

    日期常用方法

--- src/main/java/com/practise/demo/util/DateUtil.java
+++ src/main/java/com/practise/demo/util/DateUtil.java
@@ -1,8 +1,7 @@
 package com.practise.demo.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,30 +9,221 @@ import java.util.Date;
 /**
  * @author lingwang
  * @date 2021/3/15 20:14
- * 日期处理
+ * 日期处理-常用方法
+ * 此处需注意，不用try_catch
  */
 public class DateUtil {
 
-    private static Logger logger = LoggerFactory.getLogger(DateUtil.class);
+    //年月日
+    public static String YMD = "yyyy-MM-dd";
 
-
-    /**
-     * 获取当前时间的前30天
-     *
-     * @return
-     */
-    public final static String getDayMonthEnd() {
-        try {
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-            Date date = new Date();
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(date);
-            calendar.add(Calendar.DATE, -30);
-            String endDate = format.format(calendar.getTime());
-            return endDate;
-        } catch (Exception e) {
-            logger.error("日期转换错误  " + e.getMessage());
-        }
-        return null;
+    /*
+     * 校验并将string类型转成Date类型
+     * */
+    public static Date checkStringToDate(String day) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat(YMD);
+        Date date = format.parse(day);
+        return date;
     }
+
+    /*
+     * 校验并将string类型转成Date类型
+     * */
+    public static String checkDateToString(Date day, String format) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        return simpleDateFormat.format(day);
+    }
+
+
+    /*
+     * 校验并将日期转换成想要的格式
+     * */
+    public static String changeFormatString(String date, String format1, String format2) throws ParseException {
+        String endDate = "";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format1);
+        Date currentDate = simpleDateFormat.parse(endDate);
+        simpleDateFormat = new SimpleDateFormat(format2);
+        endDate = simpleDateFormat.format(currentDate);
+        return endDate;
+    }
+
+
+    /*
+     * 获取指定日期的第N天日期
+     * */
+    public static Date getDayBefore(Date date, int before) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int day_current = calendar.get(Calendar.DATE);
+        calendar.set(Calendar.DATE, day_current + before);
+        return calendar.getTime();
+    }
+
+    public static Date getDayBefore(String date, int before) throws ParseException {
+        return getDayBefore(checkStringToDate(date), before);
+    }
+
+
+    /*
+     * 获取指定日期的前N月日期
+     * */
+    public static Date getMonthBefore(Date date, int before) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int month_current = calendar.get(Calendar.DATE);
+        calendar.set(Calendar.MONTH, month_current + before - 1);
+        return calendar.getTime();
+    }
+
+    public static Date getMonthBefore(String date, int before) throws ParseException {
+        return getMonthBefore(checkStringToDate(date), before);
+    }
+
+    /*
+     * 获取周一
+     * */
+    public static Date getMonday(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.setFirstDayOfWeek(calendar.MONDAY);
+        int num = calendar.get(Calendar.DAY_OF_WEEK);
+        if (num == 1) {
+            num = 8;
+        }
+        calendar.add(Calendar.DATE, calendar.getFirstDayOfWeek() - num);
+        return calendar.getTime();
+    }
+
+    public static Date getMonday(String date) throws ParseException {
+        return getMonday(checkStringToDate(date));
+    }
+
+    /*
+     * 获取周末
+     * */
+    public static Date getSunday(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int num = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        if (num == 0) {
+            num = 7;
+        }
+        calendar.add(Calendar.DATE, -num + 7);
+        return calendar.getTime();
+    }
+
+    public static Date getSunday(String date) throws ParseException {
+        return getSunday(checkStringToDate(date));
+    }
+
+    /*
+     * 获取当月第一天
+     * */
+    public static Date getMonthFirstDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar.getTime();
+    }
+
+    public static Date getMonthFirstDay(String date) throws ParseException {
+        return getMonthFirstDay(checkStringToDate(date));
+    }
+
+    /*
+     * 获取N月前的第一天
+     * */
+    public static Date getMonthFirstDay(Date date, int before) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.MONTH, calendar.get(calendar.MONTH) + before);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar.getTime();
+    }
+
+    public static Date getMonthFirstDay(String date, int before) throws ParseException {
+        return getMonthFirstDay(checkStringToDate(date), before);
+    }
+
+    /*
+     * 获取当月最后一天
+     * */
+    public static Date getMonthLastDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_MONTH, 0);
+        return calendar.getTime();
+    }
+
+    public static Date getMonthLastDay(String date) throws ParseException {
+        return getMonthLastDay(checkStringToDate(date));
+    }
+
+    /*
+     * 获取N月前的最后一天
+     * */
+    public static Date getMonthLastDay(Date date, int before) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.MONTH, calendar.get(calendar.MONTH) + before);
+        calendar.set(Calendar.DAY_OF_MONTH, 0);
+        return calendar.getTime();
+    }
+
+    public static Date getMonthLastDay(String date, int before) throws ParseException {
+        return getMonthLastDay(checkStringToDate(date), before);
+    }
+
+    /*
+     * 获取指定年的第一天
+     * */
+    public static Date getYearFirstDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_YEAR, 1);
+        return calendar.getTime();
+    }
+
+    public static Date getYearFirstDay(String date) throws ParseException {
+        return getYearFirstDay(checkStringToDate(date));
+    }
+
+    public static Date getYearFirstDay(Date date,int num) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year_current = calendar.get(Calendar.YEAR);
+        calendar.set(Calendar.YEAR,year_current+num);
+        calendar.set(Calendar.DAY_OF_YEAR, 1);
+        return calendar.getTime();
+    }
+    public static Date getYearFirstDay(String date,int num) throws ParseException {
+        return getYearFirstDay(checkStringToDate(date),num);
+    }
+
+    /*
+     * 获取指定年的最后一天
+     * */
+    public static Date getYearLastDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_YEAR, 0);
+        return calendar.getTime();
+    }
+
+    public static Date getYearLastDay(String date) throws ParseException {
+        return getYearFirstDay(checkStringToDate(date));
+    }
+    public static Date getYearLastDay(Date date,int num) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year_current = calendar.get(Calendar.YEAR);
+        calendar.set(Calendar.YEAR,year_current+num);
+        calendar.set(Calendar.DAY_OF_YEAR, 1);
+        return calendar.getTime();
+    }
+    public static Date getYearLastDay(String date,int num) throws ParseException {
+        return getYearLastDay(checkStringToDate(date),num);
+    }
+
+
 }
--- src/test/java/com/practise/demo/DemoApplicationTests.java
+++ src/test/java/com/practise/demo/DemoApplicationTests.java
@@ -1,19 +1,74 @@
 package com.practise.demo;
 
-import com.practise.demo.service.TestService;
+import com.practise.demo.util.DateUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Autowired
-    private TestService service;
+    public static String YMD = "yyyy-MM-dd";
 
     @Test
-    void contextLoads() {
-        System.out.println(service.getPort());
+    void contextLoads() throws ParseException {
+        String date = DateUtil.checkDateToString(DateUtil.getDayBefore("2022-01-01", 01), YMD);
+        System.out.print("获取2022-01-01后一天的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getDayBefore("2022-01-01", -1), YMD);
+        System.out.print("获取2022-01-01前一天的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getMonthBefore("2022-01-01", 1), YMD);
+        System.out.print("获取2022-01-01后一个月的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getMonthBefore("2022-01-01", -1), YMD);
+        System.out.print("获取2022-01-01前一个月的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getMonday("2022-01-01"), YMD);
+        System.out.print("获取2022-01-01周一的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getSunday("2022-01-01"), YMD);
+        System.out.print("获取2022-01-01周末的日期:");
+        System.out.println(date);
+
+        date = DateUtil.checkDateToString(DateUtil.getMonthLastDay("2022-01-02"), YMD);
+        System.out.print("获取2022-01-02月末的日期:");
+        System.out.println(date);
+
+        date = DateUtil.checkDateToString(DateUtil.getMonthLastDay("2022-01-02",2), YMD);
+        System.out.print("获取2022-01-02后两月月末的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getMonthLastDay("2022-01-02",-2), YMD);
+        System.out.print("获取2022-01-02前两月月末的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getMonthLastDay("2022-01-02",2), YMD);
+        System.out.print("获取2022-01-02后两月月末的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getMonthLastDay("2022-01-02",-2), YMD);
+        System.out.print("获取2022-01-02前两月月末的日期:");
+        System.out.println(date);
+
+        date = DateUtil.checkDateToString(DateUtil.getYearFirstDay("2022-01-02"), YMD);
+        System.out.print("获取2022-01-02当年第一天的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getYearFirstDay("2022-01-02",2), YMD);
+        System.out.print("获取2022-01-02当年后两年第一天的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getYearFirstDay("2022-01-02",-2), YMD);
+        System.out.print("获取2022-01-02当年前两年第一天的日期:");
+        System.out.println(date);
+
+
+        date = DateUtil.checkDateToString(DateUtil.getYearLastDay("2022-01-02"), YMD);
+        System.out.print("获取2022-01-02当年最后一天的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getYearLastDay("2022-01-02",2), YMD);
+        System.out.print("获取2022-01-02当年后两年最后一天的日期:");
+        System.out.println(date);
+        date = DateUtil.checkDateToString(DateUtil.getYearLastDay("2022-01-02",-2), YMD);
+        System.out.print("获取2022-01-02当年前两年最后一天的日期:");
+        System.out.println(date);
     }
 
 }
